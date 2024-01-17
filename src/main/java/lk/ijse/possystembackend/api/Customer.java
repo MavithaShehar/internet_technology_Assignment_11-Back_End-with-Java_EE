4e15f90a1af08fea914014e0a8b090d9078c6110
MavithaShehar <mavithashehar@gmail.com>
2024-01-18 03:55:32 +0530

    create customer with java EE

---
+++ src/main/java/lk/ijse/possystembackend/api/Customer.java
@@ -0,0 +1,123 @@
+package lk.ijse.possystembackend.api;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.possystembackend.db.DBProcess;
+import lk.ijse.possystembackend.dto.CustomerDTO;
+import lombok.var;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "customer",urlPatterns = "/customer",
+        initParams = {
+                @WebInitParam(name = "dto-user",value = "root"),
+                @WebInitParam(name = "dto-pw",value = "1234"),
+                @WebInitParam(name = "dto-url",value = "jdbc:mysql://localhost:3306/test"),
+                @WebInitParam(name = "dto-class",value = "com.mysql.cj.jdbc.Driver")
+
+        }
+
+)
+
+public class Customer extends HttpServlet {
+
+    Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+
+        try {
+            InitialContext initialContext = new InitialContext();
+            DataSource dataSource = (DataSource) initialContext.lookup("java:comp/env/jdbc/pos");
+            System.out.println(dataSource);
+            this.connection = dataSource.getConnection();
+
+            System.out.println(connection);
+        } catch (NamingException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("post");
+        if (req.getContentType() == null ||
+                !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+
+            Jsonb jsonb = JsonbBuilder.create();
+            CustomerDTO customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+            System.out.println(customerDTO);
+            DBProcess dbProcess = new DBProcess();
+            dbProcess.saveCustomer(customerDTO, connection);
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do get");
+
+        var writer = resp.getWriter();
+        resp.setContentType("text/html");
+        var data = new DBProcess();
+        List<CustomerDTO> getData = data.getAllCustomer(connection);
+
+        Jsonb jsonb = JsonbBuilder.create();
+        String json = jsonb.toJson(getData);
+        writer.write(json);
+        writer.close();
+
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        if (req.getContentType() == null ||
+                !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+
+            Jsonb jsonb = JsonbBuilder.create();
+            var customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+            System.out.println(customerDTO);
+            var dbProcess = new DBProcess();
+            dbProcess.updateCustomer(customerDTO, connection);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("***** do delete");
+
+        if (req.getContentType() == null ||
+                !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+
+            Jsonb jsonb = JsonbBuilder.create();
+            var customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+            System.out.println(customerDTO);
+            var dbProcess = new DBProcess();
+            dbProcess.deleteCustomer(customerDTO, connection);
+        }
+    }
+
+
+}
---
+++ src/main/java/lk/ijse/possystembackend/db/DBProcess.java
@@ -0,0 +1,120 @@
+package lk.ijse.possystembackend.db;
+
+import lk.ijse.possystembackend.dto.CustomerDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBProcess {
+
+    static final Logger logger = LoggerFactory.getLogger(DBProcess.class);
+    private static final String SAVE_CUSTOMER = "INSERT INTO customer(id,name,address) VALUES(?,?,?)";
+    private static final String GET_ALL_CUSTOMER = "SELECT * FROM customer";
+    private static final String UPDATE_CUSTOMER = "UPDATE customer SET name=?,address=? WHERE id=?;";
+    private static final String DELETE_CUSTOMER = "DELETE FROM customer WHERE id = ?";
+
+
+
+    public void saveCustomer(CustomerDTO customerDTO, Connection connection) {
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(SAVE_CUSTOMER);
+            ps.setString(1, customerDTO.getId());
+            ps.setString(2, customerDTO.getName());
+            ps.setString(3, customerDTO.getAddress());
+
+            if (ps.executeUpdate() != 0) {
+                logger.info("Item saved successfully");
+                System.out.println("Data saved");
+            } else {
+                logger.info("Item saving failed");
+                System.out.println("Failed to save");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public List<CustomerDTO> getAllCustomer(Connection connection) {
+        List<CustomerDTO> customerDTOs = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(GET_ALL_CUSTOMER);
+            ResultSet resultSet = ps.executeQuery();
+
+
+            while (resultSet.next()) {
+
+                customerDTOs.add(new CustomerDTO(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3)
+
+                ));
+
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return customerDTOs;
+    }
+
+
+    public void updateCustomer(CustomerDTO customerDTO, Connection connection) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(UPDATE_CUSTOMER);
+            ps.setString(1, customerDTO.getName());
+            ps.setString(2, customerDTO.getAddress());
+            ps.setString(3, customerDTO.getId());
+
+            if (ps.executeUpdate() != 0) {
+                logger.info("Item updated successfully");
+                System.out.println("Data updated");
+            } else {
+                logger.info("Item updating failed");
+                System.out.println("Failed to update");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteCustomer(CustomerDTO customerDTO, Connection connection) {
+
+        System.out.println("######## delete customer");
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(DELETE_CUSTOMER);
+            ps.setString(1, customerDTO.getId());
+
+            if (ps.executeUpdate() != 0) {
+                logger.info("Item deleted successfully");
+                System.out.println("Data deleted");
+            } else {
+                logger.info("Item deleting failed");
+                System.out.println("Failed to delete");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+}
