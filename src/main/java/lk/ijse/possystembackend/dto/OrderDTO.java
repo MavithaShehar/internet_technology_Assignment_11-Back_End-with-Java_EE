089a548017624084586392875dc1de2ac2ff9706
MavithaShehar <mavithashehar@gmail.com>
2024-01-20 00:51:57 +0530

    rebuild GET and DELETE methord of customer

--- src/main/java/lk/ijse/possystembackend/dto/OrderDTO.java
+++ src/main/java/lk/ijse/possystembackend/dto/OrderDTO.java
@@ -14,8 +14,11 @@ public class OrderDTO {
     private String o_id ;
     private String o_date ;
     private String c_id ;
-    private String o_items;
+    private String o_items = "click";
     private double discount;
     private double amount;
+
+    private OrderItemsDTO[] itemsDTO;
+
 
 }
--- src/main/java/lk/ijse/possystembackend/filter/CORSFilter.java
+++ src/main/java/lk/ijse/possystembackend/filter/CORSFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,10 +15,13 @@ import java.io.IOException;
 public class CORSFilter extends HttpFilter {
 
     @Override
-        protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
             System.out.println("CORS Filter");
             String origin = req.getHeader("Origin");
-            if (origin.contains(getServletContext().getInitParameter("origin"))) {
+            System.out.println(origin);
+            System.out.println(origin.contains(getServletContext().getInitParameter("origin")));
+
+        if (origin.contains(getServletContext().getInitParameter("origin"))) {
                 res.setHeader("Access-Control-Allow-Origin", origin);
                 res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,HEADER");
                 res.setHeader("Access-Control-Allow-Headers", "Content-Type");
